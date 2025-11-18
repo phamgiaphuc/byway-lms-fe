7b5736fcdaf09b714f75c77b848e61801eaee5c4
Acus <acuscodinghcm@gmail.com>
2025-11-18 22:56:24 +0700

    Phuc | Add code

---
+++ src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import React from "react";
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : React.lazy(() =>
+      import("@tanstack/react-router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    );
+
+const RootLayout = () => (
+  <>
+    <div className="flex gap-2 p-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>
+      <Link to="/about" className="[&.active]:font-bold">
+        About
+      </Link>
+    </div>
+    <hr />
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
